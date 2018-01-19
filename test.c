a05a614ad3ad5c7dc15019efa90a8c15e28158da
zplett <zplett@oberlin.edu>
2018-01-19 13:39:56 -0500

    added ascii table output and handling for lines that exceed the table length

--- test.c
+++ test.c
@@ -5,3 +5,5 @@ int test_function()
 {
   //Todo: fill out this function
 }
+
+//tOdO: this is an exceedingly long todo line to make sure that when we format output the table prints an ellipses at the end of a message that runs over the line length so that everything can be contained in a line of the same size! :)
